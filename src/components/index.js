e585e6211a3d783d63283f743edeb7d473fcb2d9
Ryan Langlois <ryan@alumworks.co>
2024-02-23 15:45:21 -0500

    Add Vertical Cards

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './MantleProvider';
 export * from './HorizontalCards';
 export * from './HighlightedCard';
+export * from './VerticalCards';
---
+++ src/stories/VerticalCards.stories.js
@@ -0,0 +1,48 @@
+import { VerticalCards } from '../components';
+import { Plans } from './test_data/plans';
+import { Customer } from './test_data/customer';
+
+export default {
+  title: 'Plans/VerticalCards',
+  component: VerticalCards,
+  tags: ['autodocs'],
+  argTypes: {
+    pageWidth: {
+      options: ["default", "narrow", "full"],
+      control: { type: 'radio' },
+    },
+  },
+  args: {
+    pageWidth: "default",
+    showRecommendedBadge: true,
+    showCurrencySymbol: true,
+    showPlanIntervalToggle: false,
+    showTrialDaysAsFeature: true,
+    useShortFormPlanIntevals: true,
+    customFieldCta: null,
+    customFieldPlanRecommended: "Recommended",
+    showCustomPlans: false,
+    backUrl: "",
+    onSubscribe: () => {},
+    customer: Customer,
+    plans: Plans,
+  }
+};
+
+export const SinglePlan = {
+  args: {
+    plans: Plans.filter(plan => plan.interval !== "ANNUAL" && plan.name !== "Ultra" && plan.name !== "Advanced" && plan.name !== "Pro"),
+  }
+};
+
+export const TwoPlans = {
+  args: {
+    plans: Plans.filter(plan => plan.interval !== "ANNUAL" && plan.name !== "Ultra" && plan.name !== "Advanced"),
+  }
+};
+
+export const ThreePlans = {
+  args: {
+    plans: Plans.filter(plan => plan.interval !== "ANNUAL" && plan.name !== "Ultra"),
+  }
+};
